a213f13bd08f7f97954417e2abf99a5882754fcf
Dev Haseeb <haseeb@stack360.co>
2024-06-27 18:20:29 +0500

    refactor: parse user input template type as TemplateType

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,58 @@
 mod render;
 mod templates;
 
-use std::process;
 use clap::Parser;
-use render::TemplateExecutor;
+use render::{TemplateConfig, TemplateExecutor, TemplateType};
+use std::process;
 use templates::java_gradle;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t = String::from("java-gradle") )]
     template: String,
 
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t = String::from("sandbox") )]
     name: String,
+
+    #[arg(short, long)]
+    list: bool
+}
+
+const VALID_TEMPLATES: [&str; 6] = [
+    "c", "cpp-cmake", "javascript (or 'js')", "typescript (or 'ts')", "java-gradle", "python",
+];
+
+fn print_valid_templates() {
+    println!("Valid templates include:");
+    for template_name in VALID_TEMPLATES {
+        println!(" - {}", template_name);
+    }
 }
 
 fn main() {
     let args = Args::parse();
+    if args.list {
+        return print_valid_templates();
+    }
 
-    let java_config = java_gradle::new_java_config(&args.name);
-    let executor = match TemplateExecutor::new(args.name, java_config) {
+    let template_type = match TemplateType::from_string(&args.template) {
+        Some(t) => t,
+        None => {
+            return print_valid_templates();
+        }
+    };
+    
+    let config: TemplateConfig = match template_type {
+        TemplateType::C => java_gradle::new_java_config(&args.name), // TODO: implement actual
+        TemplateType::CppCmake => java_gradle::new_java_config(&args.name), // TODO: implement actual
+        TemplateType::Javascript => java_gradle::new_java_config(&args.name), // TODO: implement actual
+        TemplateType::Typescript => java_gradle::new_java_config(&args.name), // TODO: implement actual
+        TemplateType::JavaGradle => java_gradle::new_java_config(&args.name),
+        TemplateType::Python => java_gradle::new_java_config(&args.name), // TODO: implement actual
+    };
+    
+    let executor = match TemplateExecutor::new(args.name, config) {
         Ok(ex) => ex,
         Err(e) => {
             println!("error: {}", e);
--- src/render.rs
+++ src/render.rs
@@ -1,94 +1,156 @@
-use std::{fs::{self, File}, io::Write, path::PathBuf};
 use askama::DynTemplate;
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
+
+pub enum TemplateType {
+    C,
+    CppCmake,
+    Javascript,
+    Typescript,
+    JavaGradle,
+    Python,
+}
+
+impl TemplateType {
+    pub fn from_string<'a>(value: &'a str) -> Option<Self> {
+        match value {
+            "c" => Some(Self::C),
+            "cpp-cmake" => Some(Self::CppCmake),
+            "javascript" | "js" => Some(Self::Javascript),
+            "typescript" | "ts" => Some(Self::Typescript),
+            "java-gradle" => Some(Self::JavaGradle),
+            "python" => Some(Self::Python),
+            _ => None
+        }
+    }
+}
 
 pub struct TemplateFile {
-  pub path: PathBuf,
-  pub template: Box<dyn DynTemplate>,
+    pub path: PathBuf,
+    pub template: Box<dyn DynTemplate>,
 }
 
 pub struct TemplateConfig {
-  pub directories: Vec<PathBuf>,
-  pub gitignore: Vec<String>,
-  pub files: Vec<TemplateFile>
+    pub directories: Vec<PathBuf>,
+    pub gitignore: Vec<String>,
+    pub files: Vec<TemplateFile>,
 }
 
 pub struct TemplateExecutor {
-  pub root: PathBuf,
-  pub config: TemplateConfig,
+    pub root: PathBuf,
+    pub config: TemplateConfig,
 }
 
 impl TemplateExecutor {
-  pub fn new(project_name: String, config: TemplateConfig) -> Result<Self, String> {
-      let current_dir = match std::env::current_dir() {
-          Ok(dir) => dir,
-          Err(e) => return Err(format!("failed to detect current directory. Details: {}", e.to_string())),
-      };
+    pub fn new(project_name: String, config: TemplateConfig) -> Result<Self, String> {
+        let current_dir = match std::env::current_dir() {
+            Ok(dir) => dir,
+            Err(e) => {
+                return Err(format!(
+                    "failed to detect current directory. Details: {}",
+                    e.to_string()
+                ))
+            }
+        };
 
-      let executor = TemplateExecutor {
-          root: current_dir.join(project_name),
-          config,
-      };
+        let executor = TemplateExecutor {
+            root: current_dir.join(project_name),
+            config,
+        };
 
-      Ok(executor)
-  }
+        Ok(executor)
+    }
 
-  fn create_directories(&self) -> Result<(), String> {
-      for dir_name in self.config.directories.iter() {
-          let dir = self.root.join(dir_name);
-          let err = match fs::create_dir_all(&dir) {
-              Ok(_) => continue,
-              Err(err) => err 
-          };
+    fn create_directories(&self) -> Result<(), String> {
+        for dir_name in self.config.directories.iter() {
+            let dir = self.root.join(dir_name);
+            let err = match fs::create_dir_all(&dir) {
+                Ok(_) => continue,
+                Err(err) => err,
+            };
 
-          return Err(format!("failed to created directory '{:?}'. Details: {}", dir, err.to_string()));
-      }
+            return Err(format!(
+                "failed to created directory '{:?}'. Details: {}",
+                dir,
+                err.to_string()
+            ));
+        }
 
-      Ok(())
-  }
+        Ok(())
+    }
 
-  fn create_gitignore(&self) -> Result<(), String> {
-      let gitignore_path = self.root.join(".gitignore");
+    fn create_gitignore(&self) -> Result<(), String> {
+        let gitignore_path = self.root.join(".gitignore");
 
-      let mut file = match File::create(gitignore_path) {
-          Ok(f) => f,
-          Err(e) => return Err(format!("failed to create .gitignore file. Details: {}", e.to_string())),
-      };
+        let mut file = match File::create(gitignore_path) {
+            Ok(f) => f,
+            Err(e) => {
+                return Err(format!(
+                    "failed to create .gitignore file. Details: {}",
+                    e.to_string()
+                ))
+            }
+        };
 
-      for line in self.config.gitignore.iter() {
-          match file.write(format!("{}\n", line).as_bytes()) {
-              Ok(_) => continue,
-              Err(e) => return Err(format!("failed to write to .gitignore file. Details: {}", e.to_string())),
-          }
-      }
+        for line in self.config.gitignore.iter() {
+            match file.write(format!("{}\n", line).as_bytes()) {
+                Ok(_) => continue,
+                Err(e) => {
+                    return Err(format!(
+                        "failed to write to .gitignore file. Details: {}",
+                        e.to_string()
+                    ))
+                }
+            }
+        }
 
-      Ok(())
-  }
+        Ok(())
+    }
 
-  fn render_file_templates(&self) -> Result<(), String> {
-      for file in self.config.files.iter() {
-          let output_path = self.root.join(file.path.clone());   
-          let mut output_file = match File::create(&output_path) {
-              Ok(f) => f,
-              Err(e) => return Err(format!("failed to create output file '{:?}'. Details: {}", output_path, e.to_string())),
-          };
+    fn render_file_templates(&self) -> Result<(), String> {
+        for file in self.config.files.iter() {
+            let output_path = self.root.join(file.path.clone());
+            let mut output_file = match File::create(&output_path) {
+                Ok(f) => f,
+                Err(e) => {
+                    return Err(format!(
+                        "failed to create output file '{:?}'. Details: {}",
+                        output_path,
+                        e.to_string()
+                    ))
+                }
+            };
 
-          let content = match file.template.dyn_render() {
-              Ok(c) => c,
-              Err(e) => return Err(format!("failed to render template '{:?}'. Details: {}", output_path, e.to_string())),
-          };
+            let content = match file.template.dyn_render() {
+                Ok(c) => c,
+                Err(e) => {
+                    return Err(format!(
+                        "failed to render template '{:?}'. Details: {}",
+                        output_path,
+                        e.to_string()
+                    ))
+                }
+            };
 
-          if let Err(e) = output_file.write(content.as_bytes()) {
-              return Err(format!("failed to write to output file '{:?}'. Details: {}", output_path, e.to_string()));
-          }
-      }
+            if let Err(e) = output_file.write(content.as_bytes()) {
+                return Err(format!(
+                    "failed to write to output file '{:?}'. Details: {}",
+                    output_path,
+                    e.to_string()
+                ));
+            }
+        }
 
-      Ok(())
-  }
+        Ok(())
+    }
 
-  pub fn execute(&self) -> Result<(), String> {
-      self.create_directories()?;
-      self.create_gitignore()?;
-      self.render_file_templates()?;
-      Ok(())
-  }
+    pub fn execute(&self) -> Result<(), String> {
+        self.create_directories()?;
+        self.create_gitignore()?;
+        self.render_file_templates()?;
+        Ok(())
+    }
 }
